2eb15059b271405b576ba865d3da7cb7e7655709
dhruvintejani <156985917+dhruvintejani@users.noreply.github.com>
2026-07-07 23:30:06 +0530

    Add UI component barrel exports

---
+++ src/components/ui/index.ts
@@ -0,0 +1,8 @@
+export { default as Avatar } from './Avatar';
+export { default as Badge } from './Badge';
+export { default as Button } from './Button';
+export { default as EmptyState } from './EmptyState';
+export { default as IconButton } from './IconButton';
+export { default as Input } from './Input';
+export { default as OptimizedImage } from './OptimizedImage';
+export { default as Skeleton, SkeletonLine } from './Skeleton';
