7c2565cd072e25f7557c4f8378940b50a17f7490
dhruvintejani <156985917+dhruvintejani@users.noreply.github.com>
2026-07-08 18:30:05 +0530

    Export SEO manager

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,3 +1,4 @@
 export { default as AppErrorBoundary } from './AppErrorBoundary';
 export { default as RouteAnnouncer } from './RouteAnnouncer';
 export { default as RouteLoadingFallback } from './RouteLoadingFallback';
+export { default as Seo } from './Seo';
