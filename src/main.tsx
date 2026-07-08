ddc5e54acc09fabfae4d1b62f36d3c1b286d1a0d
dhruvintejani <156985917+dhruvintejani@users.noreply.github.com>
2026-07-08 17:52:15 +0530

    Load design system stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import './styles/theme.css';
+import './styles/design-system.css';
 import './styles/polish.css';
 import './styles/responsive.css';
 import './styles/product-polish.css';
