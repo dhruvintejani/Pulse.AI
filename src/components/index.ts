3843e029b1e4ad915b6ccbd2fde69ff10f9bc52a
dhruvintejani <156985917+dhruvintejani@users.noreply.github.com>
2026-07-08 10:05:33 +0530

    Export dashboard components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export * from './auth';
 export * from './backgrounds';
 export * from './chat';
+export * from './dashboard';
 export * from './data';
 export * from './layout';
 export * from './ui';
