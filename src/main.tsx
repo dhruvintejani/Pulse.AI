ae8f8a1639c5e38bf116bc8beee4d8c8d7666acf
dhruvintejani <156985917+dhruvintejani@users.noreply.github.com>
2026-07-08 09:59:07 +0530

    Use contexts barrel export

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import "./styles/theme.css";
-import "./styles/polish.css";
-import App from "./App";
-import AppProviders from "./contexts/AppProviders";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import './styles/theme.css';
+import './styles/polish.css';
+import App from './App';
+import { AppProviders } from './contexts';
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppProviders>
       <App />
