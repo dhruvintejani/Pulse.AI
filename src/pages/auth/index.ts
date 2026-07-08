2ee0b305c003b2bfc2813fa81e83881370ff4773
dhruvintejani <156985917+dhruvintejani@users.noreply.github.com>
2026-07-08 09:56:02 +0530

    Add auth pages barrel exports

---
+++ src/pages/auth/index.ts
@@ -0,0 +1,5 @@
+export { default as ForgotPasswordPage } from './ForgotPasswordPage';
+export { default as LoginPage } from './LoginPage';
+export { default as ResetPasswordPage } from './ResetPasswordPage';
+export { default as SignupPage } from './SignupPage';
+export { default as VerifyPage } from './VerifyPage';
