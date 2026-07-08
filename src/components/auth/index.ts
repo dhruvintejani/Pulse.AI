263645e9e0194f33cc818b9d2f096e01322c8ead
dhruvintejani <156985917+dhruvintejani@users.noreply.github.com>
2026-07-08 09:55:14 +0530

    Add auth component barrel exports

---
+++ src/components/auth/index.ts
@@ -0,0 +1,4 @@
+export { default as AuthAlert } from './AuthAlert';
+export { default as AuthLoadingScreen } from './AuthLoadingScreen';
+export { default as ProtectedRoute } from './ProtectedRoute';
+export { default as PublicRoute } from './PublicRoute';
