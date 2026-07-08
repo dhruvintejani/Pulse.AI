1637643213e24ce4051841bd26f902e0629be7bf
dhruvintejani <156985917+dhruvintejani@users.noreply.github.com>
2026-07-08 10:19:59 +0530

    Export chat preview and streaming components

--- src/components/chat/index.ts
+++ src/components/chat/index.ts
@@ -1,2 +1,4 @@
+export { default as ChatAttachmentPreview } from './ChatAttachmentPreview';
 export { default as CodeBlock } from './CodeBlock';
 export { default as MarkdownRenderer } from './MarkdownRenderer';
+export { default as StreamingMessage } from './StreamingMessage';
