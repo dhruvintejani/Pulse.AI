ecd49e6d2da64e6484ebcd3364447aea05e2e227
dhruvintejani <156985917+dhruvintejani@users.noreply.github.com>
2026-07-08 10:23:21 +0530

    Add document component barrel exports

---
+++ src/components/documents/index.ts
@@ -0,0 +1,4 @@
+export { default as DeleteDocumentDialog } from './DeleteDocumentDialog';
+export { default as DocumentCard } from './DocumentCard';
+export { default as DocumentPreviewModal } from './DocumentPreviewModal';
+export { default as DocumentUploadModal } from './DocumentUploadModal';
