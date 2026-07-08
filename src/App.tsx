7ed561dda6151d585e7f14c09e9106821cb51415
dhruvintejani <156985917+dhruvintejani@users.noreply.github.com>
2026-07-08 18:30:39 +0530

    Mount global UX and SEO shell

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { BrowserRouter } from 'react-router-dom';
-import { AppErrorBoundary, RouteAnnouncer } from '@/components/common';
+import { AppErrorBoundary, RouteAnnouncer, Seo } from '@/components/common';
+import { CommandPalette, InstallPrompt, KeyboardShortcuts, OfflineBanner } from '@/components/experience';
 import { AppRouter } from '@/routes';
 
 function App() {
   return (
     <AppErrorBoundary>
       <BrowserRouter>
+        <Seo />
         <RouteAnnouncer />
+        <KeyboardShortcuts />
+        <CommandPalette />
+        <OfflineBanner />
+        <InstallPrompt />
         <AppRouter />
       </BrowserRouter>
     </AppErrorBoundary>
