a1768722289582e9e04806c1d089f1f4a5f512c8
dhruvintejani <156985917+dhruvintejani@users.noreply.github.com>
2026-07-08 10:23:34 +0530

    Export document components

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,6 @@ export * from './backgrounds';
 export * from './chat';
 export * from './dashboard';
 export * from './data';
+export * from './documents';
 export * from './layout';
 export * from './ui';
