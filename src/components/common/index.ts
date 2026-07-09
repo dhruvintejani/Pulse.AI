d920c9ba33db93dd2ab2c8179b8210c1f7cf002b
dhruvintejani <156985917+dhruvintejani@users.noreply.github.com>
2026-07-09 19:37:33 +0530

    Export global error handlers

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,4 +1,5 @@
 export { default as AppErrorBoundary } from './AppErrorBoundary';
+export { default as GlobalErrorHandlers } from './GlobalErrorHandlers';
 export { default as RouteAnnouncer } from './RouteAnnouncer';
 export { default as RouteLoadingFallback } from './RouteLoadingFallback';
 export { default as Seo } from './Seo';
