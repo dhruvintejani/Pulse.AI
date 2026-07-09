7b7c18d2a7a03c05b3d6c488b1f42570e60b28df
dhruvintejani <156985917+dhruvintejani@users.noreply.github.com>
2026-07-09 19:35:04 +0530

    Export error components

---
+++ src/components/errors/index.ts
@@ -0,0 +1,3 @@
+export { default as ErrorIllustration } from './ErrorIllustration';
+export type { ErrorIllustrationVariant } from './ErrorIllustration';
+export { default as ErrorPageShell } from './ErrorPageShell';
